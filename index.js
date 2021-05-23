7c5bbac79360b8c94aff84bee65aacae644ac9bd
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-23 22:06:39 +0300

    commit tree a62a2628d2b350a814359894d967a054670ef4b4

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize (string) {
+    let words = string.split(' ');
+
+    for(let i = 0; i < words.length; i++){
+      let unwantedRegex = /^.*\b(the|a|an|but|of|and|for|at|by|from)\b.*$/gm;
+
+      if (i === 0) {
+        words[i] = this.capitalize(words[i]);
+      }
+      if (unwantedRegex.test(words[i])){
+        words[i] = words[i].toLowerCase();
+      }
+      else {
+        words[i] = this.capitalize(words[i]);
+      }
+    }
+    return words.join(' ');
+  }
 }
